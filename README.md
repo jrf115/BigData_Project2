45e05d9851be28b8384a961cab5d15137d8ba7ad
jrf115 <jrf115@zips.uakron.edu>
2020-04-10 19:41:22 -0400

    Create Sequential fully connected model

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BigData_Project2
  Use Keras to create and compare deep learning models for the MNIST Fashion data set. Create saved models to be reused, and create logs for visualizing in TensorBoard. Use the MNIST Fashion data set for training and validation.
+
+
+# How to run project
+Download anaconda for python and create a new tensorflow environment:
+
+	conda create -n tf2 python=3.6
+	conda activate tf2
+	conda install -c conda-gorge keras
+	conda install matplotlib
+
+After installing everything needed run the code and view the code produced logs on TensorBoard:
+
+	conda activate tf2
+	python p2_5276.py
+	tensorboard --logdir logs/fit
--- p2_5276.py
+++ p2_5276.py
@@ -1,10 +1,10 @@
 """
 John Fahringer jrf115@zips.uakron.edu
 Big Data Programming _ Project2
-All rights reserved
 """
 from __future__ import print_function
 import keras
+import datetime
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
@@ -12,8 +12,48 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import os
 
+# Use Keras to create and compare deep learning models for the MNIST Fashion data set.
+
+# Create saved models to be reused, and create logs for visualizing in TensorBoard.
+# Use the MNIST Fashion data set for training and validation.
 
 ### 1 ###
 #  Create a Sequential fully connected model. Create logs for accuracy and loss
 #  of training and validation data sets. Save the model in ‘hdf5’ format. Name the saved
 #  model as ‘f_mnist.h5’
+mnist = keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+do_Sequential = False
+if do_Sequential:
+    Seq_model = Sequential()
+    Seq_model.add(Flatten(input_shape=(28, 28)))
+    Seq_model.add(Dense(512, activation='relu'))
+    Seq_model.add(Dense(10, activation="softmax"))
+    Seq_model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    # Train the model
+    history = Seq_model.fit(x=x_train,
+                             y=y_train,
+                             epochs=5,
+                             validation_data=(x_test, y_test),
+                             callbacks=[tensorboard_callback])
+
+    Seq_Dict = history.history
+
+    Seq_acc = history.history['accuracy']
+    Seq_val_acc = history.history['val_accuracy']
+    Seq_loss = history.history['loss']
+    Seq_val_loss = history.history['val_loss']
+
+
+    # Save model
+    Seq_model.reset_metrics()
+    Seq_model.save('Models/f_mnist.h5')
