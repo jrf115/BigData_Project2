f6feeb1a541ef935fc7c2c1e0ddda679832e6920
jrf115 <jrf115@zips.uakron.edu>
2020-04-11 14:42:55 -0400

    Add cov2D model and list comparing the two current models
    One annoying thing that took me a long time was in order to get the cov2d model working, I needed to change
    
    Cov_model.compile(loss='sparse_categorical_crossentropy',
                          optimizer="adam",
                          metrics=['accuracy'])
    
    To :
    
    Cov_model.compile(loss='categorical_crossentropy',
                          optimizer="adam",
                          metrics=['accuracy'])
    
    Which was annoying because python was throwing errors about expecting something about an output? of 1 and it was receiving an output? of 10.
    Because of this I thought the model was having issues with either the test and training
    data, or the inputs/outputs of the model. Very Frustrating, as it took me all last evening and this morning to noon to figure out.
    
    And now with this commit I find I have to run each model independtly. Or else the conv2D model will fail after its first epoch.

--- p2_5276.py
+++ p2_5276.py
@@ -4,12 +4,13 @@ Big Data Programming _ Project2
 """
 from __future__ import print_function
 import keras
-import datetime
+from keras.datasets import fashion_mnist
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+import datetime
 import os
 
 # Use Keras to create and compare deep learning models for the MNIST Fashion data set.
@@ -21,13 +22,18 @@ import os
 #  Create a Sequential fully connected model. Create logs for accuracy and loss
 #  of training and validation data sets. Save the model in ‘hdf5’ format. Name the saved
 #  model as ‘f_mnist.h5’
-fashion_mnist = keras.datasets.mnist
-
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
 do_Sequential = True
 if do_Sequential:
+    print("\n\n\n__Sequential__")
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    print("Shape of fashion_mnist:", x_train.shape)
+    print("Num of labels in data:", len(y_train))
+    print("The labels...:", y_train)
+    print("Shape of testing data:", x_test.shape)
+    print("The test set contains this amount of data:", len(y_test))
+
     Seq_model = Sequential()
     Seq_model.add(Flatten(input_shape=(28, 28)))
     Seq_model.add(Dense(512, activation='relu'))
@@ -37,14 +43,14 @@ if do_Sequential:
                   metrics=['accuracy'])
 
     log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    s_tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
     # Train the model
     Seq_history = Seq_model.fit(x=x_train,
                              y=y_train,
-                             epochs=5,
+                             epochs=2,
                              validation_data=(x_test, y_test),
-                             callbacks=[tensorboard_callback])
+                             callbacks=[s_tensorboard_callback])
 
     Seq_Dict = Seq_history.history
 
@@ -55,5 +61,75 @@ if do_Sequential:
 
 
     # Save model
-    Seq_model.reset_metrics()
+    #Seq_model.reset_metrics()
     Seq_model.save('Models/f_mnist.h5')
+
+
+
+### 2 ###
+#  Create a cov2Dnet model. Create logs for accuracy and loss of training and
+#  validation data sets. Save the model in ‘hdf5’ format. Name the saved model as
+#  ‘cov_f_mnist.h5’.
+do_Convolutional = True
+if do_Convolutional:
+    print("\n\n\n__CNN__")
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+    # Reshape data'
+    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+    x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+
+    x_train, x_test = x_train.astype('float32'), x_test.astype('float32')
+    x_train, x_test = x_train / 255, x_test / 255
+
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
+
+    print("Shape of fashion_mnist:", x_train.shape)
+    print("Num of labels in data:", len(y_train))
+    print("The labels...:", y_train)
+    print("Shape of testing data:", x_test.shape)
+    print("The test set contains this amount of data:", len(y_test))
+
+    Cov_model = Sequential()
+    Cov_model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(28, 28, 1)))
+    Cov_model.add(Activation('relu'))
+
+    Cov_model.add(Conv2D(64, (3, 3), activation='relu'))
+    Cov_model.add(MaxPooling2D(pool_size=(2, 2)))
+    Cov_model.add(Dropout(0.25))
+    Cov_model.add(Flatten())
+
+    Cov_model.add(Dense(128))
+    Cov_model.add(Activation('relu'))
+    Cov_model.add(Dropout(0.5))
+
+    Cov_model.add(Dense(10))
+    Cov_model.add(Activation('softmax'))
+
+    Cov_model.compile(loss='categorical_crossentropy',
+                      optimizer="adam",
+                      metrics=['accuracy'])
+
+    c_log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=c_log_dir, histogram_freq=1)
+
+    Cov_history = Cov_model.fit(x_train, y_train,
+                                batch_size=128,
+                                epochs=2,
+                                verbose=2,
+                                validation_data=(x_test, y_test),
+                                callbacks=[tensorboard_callback])
+
+    Cov_model.save('Models/cov_f_mnist.h5')
+
+
+
+
+### 3 ###
+# List and compare the two models in terms of accuracy and hyperparameters.
+
+print("\nThe MNIST Sequential fully connected model")
+print(Seq_model.summary())
+print("\nThe MNIST Cov2Dnet Model")
+print(Cov_model.summary(), '\n')
